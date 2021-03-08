cddb0073a903546f651c62b34851ca653d0aa8f7
SamihaNoor <samiha.nandini@gmail.com>
2021-03-08 10:19:58 +0600

    checkout controller, viewModel & index

---
+++ Lab Assignment-Mid/ShoppingCartMVC/Ch24ShoppingCartMVC/Controllers/CheckoutController.cs
@@ -0,0 +1,49 @@
+﻿using Ch24ShoppingCartMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ch24ShoppingCartMVC.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        private CartModel cart = new CartModel();
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            CartViewModel model = (CartViewModel)TempData["cart"];
+            if (model == null)
+            {
+                model = cart.GetCart();
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Index(OrderViewModel order)
+        {
+            CartViewModel model = cart.GetCart(order.SelectedProduct.ProductID);
+            model.AddedProduct.Quantity = order.SelectedProduct.Quantity;
+            cart.AddToCart(model);
+            TempData["cart"] = model;
+            return View(model);
+        }
+
+        [HttpGet]
+
+        public ActionResult Checkout()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        public ActionResult Submit()
+        {
+            return RedirectToAction("Checkout", "Checkout");
+        }
+
+    }
+}
---
+++ Lab Assignment-Mid/ShoppingCartMVC/Ch24ShoppingCartMVC/Models/ViewModels/CheckoutViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Ch24ShoppingCartMVC.Models.ViewModels
+{
+    public class CheckoutViewModel
+    {
+        [Required(ErrorMessage="First Name required")]
+        [Display(Name="First Name")]
+        public string fName;
+
+        [Required(ErrorMessage = "Last Name required")]
+        [Display(Name = "Last Name")]
+        public string lName;
+
+        [Required(ErrorMessage = "Email is required")]
+        public string email;
+
+        [Required(ErrorMessage = "Address is required")]
+        public string address;
+
+        [Required(ErrorMessage = "City is required")]
+        public string city;
+
+        [Required(ErrorMessage = "State is required")]
+        public string state;
+
+        [Required(ErrorMessage = "Zip Code is required")]
+        public int zipCode;
+
+        [Required(ErrorMessage = "Contact information is required")]
+        public string contact;
+    }
+}
